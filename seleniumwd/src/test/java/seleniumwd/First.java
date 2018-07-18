d0d3f1c9368b1ba9a937c660f6fee97ed6e68bb5
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-3XYKX52.dir.svc.accenture.com>
2018-07-18 11:57:06 +0530

    save and commit with changes using testng suite

--- seleniumwd/src/test/java/seleniumwd/First.java
+++ seleniumwd/src/test/java/seleniumwd/First.java
@@ -4,14 +4,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+
 public class First {
+	@Parameters("browser")
 	@Test
-	 public void testlogin() throws InterruptedException
+	 public void testlogin(String value)
 	 {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\training_b8c.07.19\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver= null;
+		if(value.equals("chrome1"))
+		{
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\training_b8c.07.19\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		else if (value.equals("chrome2"))
+		{
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\training_b8c.07.19\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		else
+		{
+			System.out.println("First install driver");
+		}
 		driver.get("https://www.Google.com");
 		driver.findElement(By.className("gb_P")).click();
 		String expected = "The world’s leading software development platform · GitHub";
